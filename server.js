4abf8ea7977ee034f65e17a8aa92f28b828217c0
Ajinkya Rajguru <ajinkya.a.rajguru@gmail.com>
2021-12-27 18:06:10 -0800

    Format on save enabled

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.use('/',(req, res) => {
-    res.json({data: {message: 'Hello from JOSN'}})
+app.use('/', (req, res) => {
+    res.json({ data: { message: 'Hello from JOSN' } })
 })
 
-app.listen(1337, ()=>{
-    console.log('This is the server')
-})
+app.listen(1337, () => {
+    console.log('This is the server');
+});
